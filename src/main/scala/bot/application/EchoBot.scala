a8721d889c4d429b014e08ae8a2c4af6b04cce16
y_xiao <y_xiao@septeni-original.co.jp>
2016-12-28 17:25:14 +0900

    add header signature check directive

--- src/main/scala/bot/application/EchoBot.scala
+++ src/main/scala/bot/application/EchoBot.scala
@@ -1,15 +1,34 @@
 package bot.application
 
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Directive0, Route}
 import bot.application.json.EventsJsonSupport
 import bot.line.model.Events
 
-class EchoBot(val replyService: ReplyService) extends EventsJsonSupport {
+class EchoBot(val channelSecret: String, val replyService: ReplyService) extends EventsJsonSupport {
+
+  def verifySignature(channelSecret: String): Directive0 =
+    headerValueByName("X-Line-Signature").flatMap { signature =>
+      entity(as[String]).flatMap { bodyString =>
+        val key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256")
+        val mac = Mac.getInstance("HmacSHA256")
+        mac.init(key)
+        val source = mac.doFinal(bodyString.getBytes(StandardCharsets.UTF_8))
+        val signatureN = Base64.getEncoder.encodeToString(source)
+
+        if (signature == signatureN) pass
+        else reject
+      }
+    }
 
   def routes: Route = {
     path("line" / "callback") {
-      (post & entity(as[Events])) { request =>
+      (post & verifySignature(channelSecret) & entity(as[Events])) { request =>
         request.events.map { event =>
           val replyToken = event.replyToken
           val message = event.message.text
