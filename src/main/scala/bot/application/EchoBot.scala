d92e02b3a3d8a36b417e33145ac72fe94017f89a
xoyo24 <xoyo24@gmail.com>
2016-12-29 09:19:03 +0900

    refactored
    - move verifySignature to BaseBot
    - use pattern matching Message type

--- src/main/scala/bot/application/EchoBot.scala
+++ src/main/scala/bot/application/EchoBot.scala
@@ -1,46 +1,19 @@
 package bot.application
 
-import java.nio.charset.StandardCharsets
-import java.util.Base64
-import javax.crypto.Mac
-import javax.crypto.spec.SecretKeySpec
-
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Directive0, Route}
-import bot.application.json.EventsJsonSupport
-import bot.line.model.Events
+import akka.http.scaladsl.server.Route
+import bot.line.model.{Events, LINETextMessageEvent}
 
-class EchoBot(val channelSecret: String, val replyService: ReplyService) extends EventsJsonSupport {
-
-  def verifySignature(channelSecret: String): Directive0 =
-    headerValueByName("X-Line-Signature").flatMap { signature =>
-      entity(as[String]).flatMap { bodyString =>
-        if (computeSignature(channelSecret, bodyString) == signature) pass
-        else reject
-      }
-    }
-
-  private def computeSignature(channelSecret: String, bodyString: String) = {
-    val key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256")
-    val mac = Mac.getInstance("HmacSHA256")
-    mac.init(key)
-    val source = mac.doFinal(bodyString.getBytes(StandardCharsets.UTF_8))
-    val signatureN = Base64.getEncoder.encodeToString(source)
-    signatureN
-  }
+class EchoBot(val channelSecret: String, val replyService: ReplyService) extends BaseBot {
 
   def routes: Route = {
-    path("line" / "callback") {
-      (post & verifySignature(channelSecret) & entity(as[Events])) { request =>
-        request.events.map { event =>
-          val replyToken = event.replyToken
-          val message = event.message.text
-
-          replyService.replyMessage(replyToken, message)
-        }
-        complete {
-          "OK"
-        }
+    (path("line" / "callback") & post & verifySignature(channelSecret) & entity(as[Events])) { body =>
+      body.events.collect {
+        case LINETextMessageEvent(replyToken, _, _, _, message) =>
+          replyService.replyMessage(replyToken, message.text)
+      }
+      complete {
+        "OK"
       }
     }
   }
